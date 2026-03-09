4a2e76d12c85e36caeed1f1b6a6a1163861bc03c
kan <kanishk.s2015@gmail.com>
2026-03-09 19:43:05 +0530

    Initial Commit

---
+++ Sierra/SierraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SierraApp.swift
+//  Sierra
+//
+//  Created by kan on 09/03/26.
+//
+
+import SwiftUI
+
+@main
+struct SierraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
